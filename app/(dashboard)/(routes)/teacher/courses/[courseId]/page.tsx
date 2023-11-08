3798f782961b7449691f028658b2ded8bee3a2a6
TwilightLogic <twilightlogic@gmail.com>
2023-11-08 23:40:49 +0800

    wip(course): build course page based on courseId

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,51 @@
-const CourseIdPage = ({ params }: { params: { courseId: string } }) => {
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+    const { userId } = auth()
+
+    if (!userId) {
+        return redirect('/')
+    }
+
+    const course = await db.course.findUnique({
+        where: { id: params.courseId },
+    })
+
+    if (!course) {
+        return redirect('/')
+    }
+
+    const requireFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId,
+    ]
+
+    const totalFields = requireFields.length
+    const completedFields = requireFields.filter(Boolean).length
+    const completionText = `(${completedFields}/${totalFields})`
+
     return (
-        <div>
-            <h1>Course Id: {params.courseId}</h1>
+        <div className='p-6'>
+            <div className='flex items-center justify-between'>
+                <div className='flex flex-col gap-y-2'>
+                    <h1 className='text-2xl font-medium'>Course setup</h1>
+                    <span className='text-sm text-slate-700 dark:text-slate-500'>
+                        Complete all fields {completionText}
+                    </span>
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
+                <div>
+                    <div className='flex items-center gap-x-2'>
+                        <h2 className='text-xl'>Customize your course</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -50,7 +50,7 @@ const CreatePage = () => {
         <div className='max-w-5xl mx-auto flex-col md:items-center md:justify-center h-full p-6'>
             <div>
                 <h1 className='text-2xl'>Name your course</h1>
-                <p className='text-sm text-slate-600'>
+                <p className='text-sm text-slate-600 dark:text-slate-500'>
                     What would you like to name to your course? Don&apos;t
                     worry, you can change this later.
                 </p>
