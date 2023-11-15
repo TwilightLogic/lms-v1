49df417ad4bdb79e5555575b777bb0d0ed3e278f
TwilightLogic <twilightlogic@gmail.com>
2023-11-15 12:22:14 +0800

    feat(teacher): add logic for course

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,5 +1,20 @@
 'use client'
 
+import * as z from 'zod'
+import axios from 'axios'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
 interface TitleFormProps {
     initialData: {
         title: string
@@ -7,6 +22,21 @@ interface TitleFormProps {
     courseId: string
 }
 
+const formSchema = z.object({
+    title: z.string().min(1, { message: 'Title is required' }),
+})
+
 export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData,
+    })
+
+    const { isSubmitting, isValid } = form.formState
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log('values', values)
+    }
+
     return <div>Title form</div>
 }
