505ec50cf9b3f102fce4456c0e01dfbb2d9c2015
TwilightLogic <twilightlogic@gmail.com>
2023-12-11 21:52:08 +0800

    Create chapters-list component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -20,6 +20,7 @@ import { useRouter } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import { Chapter, Course } from '@prisma/client'
 import { Input } from '@/components/ui/input'
+import { ChaptersList } from './chapters-list'
 
 interface ChaptersFormProps {
     initialData: Course & { chapters: Chapter[] }
@@ -111,7 +112,11 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
                     )}
                 >
                     {!initialData.chapters.length && 'Np chapters'}
-                    {/* TODO: Add a list of chapters */}
+                    <ChaptersList
+                        onEdit={() => {}}
+                        onReorder={() => {}}
+                        items={initialData.chapters || []}
+                    />
                 </div>
             )}
             {!isCreating && (
