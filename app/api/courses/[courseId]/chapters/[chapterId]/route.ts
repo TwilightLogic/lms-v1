df9ccf3a32cca7cb5a01d6ded00c781fe88c3020
TwilightLogic <twilightlogic@gmail.com>
2023-12-29 23:30:52 +0800

    cl(app): complete the function of uploading video to MUX

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,12 @@
 import { db } from '@/lib/db'
+import Mux from '@mux/mux-node'
 import { auth } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
+
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID!,
+  process.env.MUX_TOKENS_SECRET!,
+)
 
 export async function PATCH(
   req: Request,
@@ -27,7 +33,32 @@ export async function PATCH(
       data: { ...values },
     })
 
-    // TODO: Handle Video Upload
+    // Upload the video to Mux
+    if (values.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: { chapterId: params.chapterId },
+      })
+
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId)
+        await db.muxData.delete({ where: { id: existingMuxData.id } })
+      }
+
+      const asset = await Video.Assets.create({
+        input: values.videoUrl,
+        playback_policy: 'public',
+        test: false,
+      })
+
+      await db.muxData.create({
+        data: {
+          chapterId: params.chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0]?.id!,
+        },
+      })
+    }
+
     return NextResponse.json(chapter)
   } catch (error) {
     console.log('[COURSE_CHAPTER_ID', error)
