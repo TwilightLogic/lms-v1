3eed945e8d4c01fe0244a124fc15c412c1c4e7b1
TwilightLogic <twilightlogic@gmail.com>
2024-01-01 16:51:37 +0800

    feat(app): implement the confitte animation

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/actions.tsx
@@ -2,6 +2,7 @@
 
 import { ConfirmModal } from '@/components/modals/confirm-modal'
 import { Button } from '@/components/ui/button'
+import { useConfettiStore } from '@/hooks/use-confetti-store'
 import axios from 'axios'
 import { Trash } from 'lucide-react'
 import { useRouter } from 'next/navigation'
@@ -17,6 +18,8 @@ interface ActionsProps {
 export const Actions = ({ disabled, courseId, isPublished }: ActionsProps) => {
   const router = useRouter()
 
+  const confetti = useConfettiStore()
+
   const [isLoading, setIsLoading] = useState(false)
 
   const onClick = async () => {
@@ -29,6 +32,7 @@ export const Actions = ({ disabled, courseId, isPublished }: ActionsProps) => {
       } else {
         await axios.patch(`/api/courses/${courseId}/publish`)
         toast.success('Course published.')
+        confetti.onOpen()
       }
 
       router.refresh()
