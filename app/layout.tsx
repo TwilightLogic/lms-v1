89d28025924b22e0f933890d621d0984b20cb407
TwilightLogic <twilightlogic@gmail.com>
2024-01-01 14:43:44 +0800

    feat(app): add zustand and react-confetti

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ToastProvider } from '@/components/providers/toast-provider'
 import { ThemeProvider } from '@/components/theme-provider'
+import { ConfettiProvider } from '@/components/providers/confetti-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         </head>
         <body className={inter.className}>
           <ThemeProvider attribute='class' defaultTheme='system'>
+            <ConfettiProvider />
             <ToastProvider />
             {children}
           </ThemeProvider>
