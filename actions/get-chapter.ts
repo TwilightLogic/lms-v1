94c804d746bc743efd1e05a1ff739740ec575a7b
TwilightLogic <twilightlogic@gmail.com>
2024-01-17 13:50:32 +0800

    feat(app): complete the `get-chapter` function

---
+++ actions/get-chapter.ts
@@ -0,0 +1,102 @@
+import { db } from '@/lib/db'
+import { Attachment, Chapter, UserProgress } from '@prisma/client'
+import { error } from 'console'
+
+interface GetChapterProps {
+  userId: string
+  courseId: string
+  chapterId: string
+}
+
+export const getChapter = async ({
+  userId,
+  courseId,
+  chapterId,
+}: GetChapterProps) => {
+  try {
+    // note:
+    // - you are going to figure out
+    // - what are the `purchase`, `course`, `chapter` below
+
+    const purchase = await db.purchase.findUnique({
+      where: {
+        userId_courseId: {
+          userId,
+          courseId,
+        },
+      },
+    })
+
+    const course = await db.course.findUnique({
+      where: {
+        isPublished: true,
+        id: courseId,
+      },
+      select: {
+        price: true,
+      },
+    })
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: chapterId,
+        isPublished: true,
+      },
+    })
+
+    console.log('[GET_CHAPTER]', { purchase, course, chapter })
+
+    if (!chapter || !course) {
+      throw error('Chapter or course not found')
+    }
+
+    let muxData = null
+    let attachments: Attachment[] = []
+    let nextChapter: Chapter | null = null
+
+    if (purchase) {
+      attachments = await db.attachment.findMany({
+        where: {
+          courseId: courseId,
+        },
+      })
+    }
+
+    if (chapter.isFree || purchase) {
+      muxData = await db.muxData.findUnique({ where: { chapterId: chapterId } })
+
+      nextChapter = await db.chapter.findFirst({
+        where: {
+          courseId: courseId,
+          isPublished: true,
+          position: { gt: chapter?.position },
+        },
+        orderBy: {
+          position: 'asc',
+        },
+      })
+    }
+
+    const UserProgress = await db.userProgress.findUnique({
+      where: {
+        userId_chapterId: { userId, chapterId },
+      },
+    })
+
+    return {
+      chapter,
+    }
+  } catch (error) {
+    console.log('[GET_CHAPTER]', error)
+
+    return {
+      chapter: null,
+      course: null,
+      muxData: null,
+      attachments: [],
+      nextChapter: null,
+      userProgress: null,
+      purchase: null,
+    }
+  }
+}
