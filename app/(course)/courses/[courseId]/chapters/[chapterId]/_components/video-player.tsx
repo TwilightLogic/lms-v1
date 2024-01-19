38fb1ea579394645e2b0ead48372f048beb28ec7
TwilightLogic <twilightlogic@gmail.com>
2024-01-19 12:16:13 +0800

    feat(app): finish the chpater HLS viedeo player

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import axios from 'axios'
+import MuxPlayer from '@mux/mux-player-react'
+import { useState } from 'react'
+import { toast } from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
+import { Loader2, Lock } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import { useConfettiStore } from '@/hooks/use-confetti-store'
+
+interface VideoPlayerProps {
+  playbackId: string
+  courseId: string
+  chapterId: string
+  nextChapterId?: string
+  isLocked: boolean
+  completeOnEnd: boolean
+  title: string
+}
+
+export const VideoPlayer = ({
+  playbackId,
+  courseId,
+  chapterId,
+  nextChapterId,
+  isLocked,
+  completeOnEnd,
+  title,
+}: VideoPlayerProps) => {
+  const [isReady, setIsReady] = useState(false)
+
+  // TODO:8:54
+  return (
+    <div className='relative aspect-video'>
+      {!isReady && (
+        <div className='absolute inset-0 flex items-center justify-center bg-slate-800'>
+          <Loader2 className='h-8 w-8 animate-spin text-secondary dark:text-slate-200' />
+        </div>
+      )}
+      {isLocked && (
+        <div className='absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary dark:text-slate-200'>
+          <Lock className='h-8 w-8' />
+          <p className='text-sm'>This chapter is locked</p>
+        </div>
+      )}
+      {!isLocked && (
+        <MuxPlayer
+          title={title}
+          className={cn(!isReady && 'hidden')}
+          onCanPlay={() => setIsReady(true)}
+          onEnded={() => {}}
+          playbackId={playbackId}
+          autoPlay
+        />
+      )}
+    </div>
+  )
+}
