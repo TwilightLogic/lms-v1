44c9ae9632915b4bd1e5960ba56829900982d564
TwilightLogic <twilightlogic@gmail.com>
2024-02-27 11:56:30 +0800

    chore: update the DATABASE_URL field

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,4 +1,17 @@
-const AnalyticsPage = () => {
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { getAnalytics } from '@/actions/get-analytics'
+
+// TODO: 10:15
+const AnalyticsPage = async () => {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect('/')
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId)
+
   return (
     <div>
       <h1>Analytics Page</h1>
